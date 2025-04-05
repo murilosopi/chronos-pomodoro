919ed1c3e299d741d2f43392346836cfbd1c0ef7
murilosopi <dev.murilosopi@gmail.com>
2025-04-05 18:05:56 -0300

    feat: add onClick handler to Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,14 @@
+import React from "react";
 import styles from "./Button.module.css";
 
 type ButtonProps = {
   children: React.ReactNode;
   title?: string;
+  onClick?: (e: React.MouseEvent) => void | undefined;
 };
 
-export const Button = ({ children = "", title = "" }: ButtonProps) => (
-  <button title={title} className={styles.button}>
+export const Button = ({ children = "", title = "", onClick }: ButtonProps) => (
+  <button title={title} className={styles.button} onClick={onClick}>
     {children}
   </button>
 );
