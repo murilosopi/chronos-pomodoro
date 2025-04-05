d82332c671b3ff973bc756432af24bfca965178f
murilosopi <dev.murilosopi@gmail.com>
2025-04-05 17:55:38 -0300

    feat: add Menu layout component and constant

---
+++ src/constants/menu.ts
@@ -0,0 +1,9 @@
+export const menuTitles = {
+  home: 'Página Inicial',
+  history: 'Histórico de Tarefas',
+  settings: 'Ajustes',
+  theme: {
+    dark: 'Mudar para Tema Claro',
+    light: 'Mudar para Tema Escuro'
+  }
+}
---
+++ src/layouts/Menu/index.tsx
@@ -0,0 +1,24 @@
+import { HistoryIcon, HomeIcon, SettingsIcon, SunIcon } from "lucide-react";
+import { Button } from "../../components/Button";
+import styles from "./Menu.module.css";
+import { menuTitles } from "../../constants/menu";
+
+export const Menu = () => (
+  <div className={styles.menu}>
+    <Button title={menuTitles.home}>
+      <HomeIcon />
+    </Button>
+
+    <Button title={menuTitles.history}>
+      <HistoryIcon />
+    </Button>
+
+    <Button title={menuTitles.settings}>
+      <SettingsIcon />
+    </Button>
+
+    <Button title={menuTitles.theme.dark}>
+      <SunIcon />
+    </Button>
+  </div>
+);
