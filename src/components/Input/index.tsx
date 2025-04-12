0091cd40a87b17466a16c63c6b797fa86cffeca8
murilosopi <dev.murilosopi@gmail.com>
2025-04-12 13:19:59 -0300

    feat: add onEnter function event to Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,20 +5,27 @@ type InputProps = {
   type?: string;
   placeholder?: string;
   label?: string;
+  onEnter?: React.KeyboardEventHandler;
 };
 
 export const Input = ({
   type = "text",
   placeholder = "",
   label = "",
+  onEnter = () => {},
 }: InputProps) => {
   const id = useRef(useId());
+
+  const keyDownHandler = (e: React.KeyboardEvent) => {
+    return e.key == "Enter" && onEnter(e);
+  };
 
   return (
     <>
       {!!label && <label htmlFor={id.current}>{label}</label>}
       <input
         className={styles.input}
+        onKeyDown={keyDownHandler}
         type={type}
         placeholder={placeholder}
         id={id.current}
