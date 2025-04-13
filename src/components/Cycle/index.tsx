b1e30782b78a1b7726b6c6d21bbc30fd01c2ebbe
murilosopi <dev.murilosopi@gmail.com>
2025-04-13 19:38:00 -0300

    fix: variant class of Cycle component

--- src/components/Cycle/index.tsx
+++ src/components/Cycle/index.tsx
@@ -8,7 +8,7 @@ type CycleProps = {
 
 export const Cycle = ({ type }: CycleProps) => {
   const title = cycleTypesDefinitions[type].title;
-  const variant = styles[`cycle--${type}"`];
+  const variant = styles[`cycle--${type}`];
 
   return <span className={`${styles.cycle} ${variant}`} title={title} />;
 };
