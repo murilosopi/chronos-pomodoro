ac425dcc3c46141695bbffa62a3270e15e974715
murilosopi <dev.murilosopi@gmail.com>
2025-04-13 20:40:31 -0300

    fix: add keys to boldify map

--- src/utils/text.tsx
+++ src/utils/text.tsx
@@ -2,5 +2,5 @@ export const boldify = (text: string): React.ReactNode =>
   text
     .split("**")
     .map((fragment, index) =>
-      index % 2 != 0 ? <strong>{fragment}</strong> : fragment
+      index % 2 != 0 ? <strong key={index}>{fragment}</strong> : fragment
     );
