c327e3e3eea7560e14cfb1270e4a63bd70ebd318
murilosopi <dev.murilosopi@gmail.com>
2025-04-14 14:48:48 -0300

    feat: add running and clone methods to CycleModel

--- src/models/CycleModel.ts
+++ src/models/CycleModel.ts
@@ -6,8 +6,8 @@ export class CycleModel {
   id: string = uuidv4();
   type: CycleTypes;
   startDate: Date = new Date();
-  completeDate?: Date;
-  interruptDate?: Date;
+  completeDate: Date | null = null;
+  interruptDate: Date | null = null;
 
   constructor({ type, taskName }: { type: CycleTypes; taskName: string }) {
     this.type = type;
@@ -24,5 +24,25 @@ export class CycleModel {
 
   interrupted(): boolean {
     return !!this.interruptDate;
+  }
+
+  running(): boolean {
+    return !this.interruptDate && !this.completeDate && !!this.startDate;
+  }
+
+  clone(): CycleModel {
+    const newCycleModel = new CycleModel({
+      taskName: this.taskName,
+      type: this.type,
+    });
+
+    Object.assign(newCycleModel, {
+      ...this,
+      startDate: this.startDate ? new Date(this.startDate) : null,
+      completeDate: this.completeDate ? new Date(this.completeDate) : null,
+      interruptDate: this.interruptDate ? new Date(this.interruptDate) : null,
+    });
+
+    return newCycleModel;
   }
 }
